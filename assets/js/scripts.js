eae7b5b15f2f1714e59325a8489aee298021503a
Jonathan Davis <trunten@btinternet.com>
2022-12-09 10:56:48 +0000

    Added popup confirmation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,4 +5,11 @@ function copy(e) {
     } catch (err) {}
     navigator.clipboard.writeText(s);
     // console.log("copied", s)
+    
+    // Get the snackbar DIV
+    let x = document.getElementById("snackbar");
+    // Add the "show" class to DIV
+    x.className = "show";
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 } 
